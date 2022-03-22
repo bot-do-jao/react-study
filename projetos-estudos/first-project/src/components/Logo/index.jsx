32584a9ee232414fcfcfc4c98f4828fd16aa5869
Joao Pedro <joaopedrobsb3@gmail.com>
2022-03-22 12:13:43 -0300

    react is way too heavy, code exists but takes too much to load

---
+++ projetos-estudos/first-project/src/components/Logo/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css';
+
+export default function Logo(){
+    return(
+        <h1 className="logo">
+            <span>FIRST </span>
+            PROJECT
+        </h1>
+    );
+}
